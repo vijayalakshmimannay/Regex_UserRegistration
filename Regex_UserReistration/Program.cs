08c88f292c9e2070490077813fc5cbbd2059ccfd
vijayalakshmimannay <vijayalakshmimannay@gmail.com>
2022-06-13 13:43:55 +0530

    [vijayalakshmi] add first name

---
+++ Regex_UserReistration/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace RegexProblems
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+
+
+            RegExpression regex = new RegExpression();
+            Console.WriteLine("Enter Name to check:");
+            string name = Console.ReadLine();
+            regex.RegexNameChecker("Prasu");
+        }
+    }
+}
---
+++ Regex_UserReistration/RegExpression.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Text.RegularExpressions;
+
+namespace RegexProblems
+{
+    internal class RegExpression
+    {
+        const string NAME = "^[A-Z]{1}[a-z]{2,}$";
+        public void RegexNameChecker(string firstName)
+        {
+            if (Regex.IsMatch(firstName, NAME))
+            {
+                Console.WriteLine("First name is valid.");
+                return;
+            }
+            Console.WriteLine("First name is not valid.");
+        }
+
+    }
+}
